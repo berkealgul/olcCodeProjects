ea96503c02a536909cc5f0ccb7b74f83237400a0
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2022-05-05 05:52:53 +0300

    Add files via upload

--- QuadTrees.cpp
+++ QuadTrees.cpp
@@ -1,67 +1,130 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
+#include <vector>
 
-struct Rect
+using namespace std;
+
+
+struct Ball
 {
-	int x, y;
+	int x, y, w, h;
+
+	Ball()
+	{
+		
+	}
 };
+
 
 class QuadTree
 {
 public:
+	static const int MAX_DEPTH = 5;
+
+	int x, y; // middle point of quadtree
+	int w, h; // half width-height of the boundaries
+	int depth;
 	QuadTree** leafs;
-	int* aBounds; // mid x-y
-	Rect* rect;
+	vector<Ball*> balls;
 
-	QuadTree(int *aBounds_, Rect *rect_)
+	QuadTree(int x, int y, int w, int h, int depth)
 	{
-		aBounds = aBounds_;
-		rect = rect_;
+		this->x = x;
+		this->y = y;
+		this->w = w;
+		this->h = h;
+		this->depth = depth;
 	}
 
-	void addRect(Rect* rect)
+	void addBall(Ball* ball)
 	{
-		if(!leafs)
-			leafs = new QuadTree * [4];
+		if (depth < QuadTree::MAX_DEPTH)
+		{
+			if (!leafs) { leafs = new QuadTree * [4]; }
 
-		int idx = getLeafIdx(rect);
+			int idx = getReigonIdx(ball);
 
-		if (leafs[idx])
-			leafs[idx]->addRect(rect);
+			if (!leafs[idx]) { leafs[idx] = createTree(idx); }
+
+			leafs[idx]->addBall(ball);
+		}
 		else
-			leafs[idx] = new QuadTree();
-
+			balls.push_back(ball);
 	}
 
-	int getLeafIdx(Rect* rect)
+	QuadTree* createTree(int idx)
 	{
-		return (rect->x < aBounds[0]) + (rect->y < aBounds[1]);
+		int w_ = w / 2;
+		int h_ = h / 2;
+
+		switch (idx)
+		{
+		case 0:
+			return new QuadTree(x-w_, y-h_, w_, h_, depth + 1);
+		case 1:
+			return new QuadTree(x + w_, y - h_, w_, h_, depth + 1);
+		case 2:
+			return new QuadTree(x - w_, y + h_, w_, h_, depth + 1);
+		case 3:
+			return new QuadTree(x + w_, y+- h_, w_, h_, depth + 1);
+		}
 	}
 
+	int getReigonIdx(Ball* ball)
+	{
+		int id = 0;
+
+		if (ball->x > x) { id++; }
+		if (ball->y > y) { id+=2;}
+
+		return id;
+	}
 
 };
-
 
 class Example : public olc::PixelGameEngine
 {
 public:
+	QuadTree* tree;
+	vector<Ball> balls;
+
 	Example()
 	{
-		sAppName = "Example";
+		sAppName = "QuadTrees";
+
+		generateBalls(10);
 	}
 
-public:
 	bool OnUserCreate() override
 	{
-		return true;
+
+		return true;;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+		Clear(olc::DARK_BLUE);
+		int w = ScreenWidth() / 2;
+		int h = ScreenHeight() / 2;
+		tree = new QuadTree(w, h, w, h, 0);
 
+
+		for (Ball b : balls)
+			tree->addBall(&b);
+
+		delete tree;
 
 		return true;
 	}
+
+	void generateBalls(int n)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			balls.push_back(Ball());
+		}
+	}
+
 };
 
 
