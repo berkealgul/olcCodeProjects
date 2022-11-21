a12a7502b7daae0d6b04305158c3240ed335cde8
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2022-11-21 07:04:08 +0300

    Update fluidSim.cpp

--- fluidSim.cpp
+++ fluidSim.cpp
@@ -255,7 +255,8 @@ public:
 
 		angle += (3.14 / 3) * fluid->dt;
 		fluid->addVelocity(cx, cy, cos(angle)*0.2, cos(angle)*0.2);
-
+		
+		Clear(olc::Black);
 		for (int x = 0; x < ScreenWidth(); x++)
 			for (int y = 0; y < ScreenHeight(); y++)
 			{
